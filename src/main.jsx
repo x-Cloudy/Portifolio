0baf6f5896db943a6907ba6e98efb830afbc61a6
Cloudy <cloudibr@gmail.com>
2024-02-27 12:21:59 -0300

    particle import fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { PageProvider } from './assets/Context/PageContext.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PageProvider>
