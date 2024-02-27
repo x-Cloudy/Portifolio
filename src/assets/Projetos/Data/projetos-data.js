f29a89f5258a5252ecc401e3e12fc661753554d5
Cloudy <cloudibr@gmail.com>
2024-02-27 18:54:22 -0300

    responsividade pag proj

--- src/assets/Projetos/Data/projetos-data.js
+++ src/assets/Projetos/Data/projetos-data.js
@@ -16,4 +16,19 @@ export const data = [
     link: 'https://x-cloudy.github.io/Mov/',
     side: true
   },
+  {
+    id: 2,
+    image: movcont,
+    name: 'MovCont (em desenvolvimento)',
+    type: 'Comisão - Design & Development',
+    desc: 'Empresa de contabilidade, focada na criação de contéudo online.',
+    tec: {
+      react: true,
+      data: true,
+      figma: true,
+      redux: false
+    },
+    link: 'https://x-cloudy.github.io/Mov/',
+    side: true
+  },
 ]
